76d0914e53d95d32c6b710908b9b70dcbb699387
Nesterenko <93931811+Danya3213@users.noreply.github.com>
2025-06-04 16:10:57 +0300

    problom fix

--- src/ContactComponents/Contacts/List/List.jsx
+++ src/ContactComponents/Contacts/List/List.jsx
@@ -9,17 +9,18 @@ export default class List extends Component {
   render() {
     return (
       <ul className="wrapper__list">
-        {this.props.contacts.length > 0 ?
-          this.props.contacts.map((item) => (
-            <Item
-              key={item.id}
-              id={item.id}
-              name={item.name}
-              surname={item.surname}
-              phone={item.phone}
-              onClick={this.props.onClick}
-            />
-          )): ""}
+        {this.props.contacts && this.props.contacts.length > 0
+          ? this.props.contacts.map((item) => (
+              <Item
+                key={item.id}
+                id={item.id}
+                name={item.name}
+                surname={item.surname}
+                phone={item.phone}
+                onClick={this.props.onClick}
+              />
+            ))
+          : ""}
       </ul>
     );
   }
